ce6b5edc40a6afbe9d96b993e5e784e8391c2353
Diego Villamil <diego.v@bineo.com>
2023-06-08 11:17:57 -0600

    Agregar Feather the specificaciones para las tablas de PROD.

--- config.py
+++ config.py
@@ -1,13 +1,36 @@
+import os
 
-from epic_py import TypeHandler
+from epic_py.identity import EpicIdentity
 
 
-fiserv_types = {  
-    'date' : { 'format': 'yyyyMMdd'}, 
-    'ts'   : { 'format': 'hhmmss'}
+ENV = os.getenv('ENV_TYPE')
+SERVER = os.getenv('SERVER_TYPE')
+
+SETUP_KEYS = {
+    'qas': {
+        'service-principal': { # oauth-databricks-qas
+            'client_id'      : 'QAS_SP_CLIENT',
+            'client_secret'  : 'QAS_SP_SECRET', 
+            'subscription_id': 'QAS_SP_SUBSTN', 
+            'tenant_id'      : 'AAD_TENANT'}, 
+    }
+}
+
+AZURE_RESOURCES = {
+    'qas': {
+        'keyvault' : 'kv-cx-data-qas',
+        'storage'  : 'stglakehyliaqas', 
+        'blob_path': "ops/fraud-prevention"}
 }
 
 
+app_agent = EpicIdentity.create(server=SERVER, config=SETUP_KEYS[ENV])
+app_resourcer = app_agent.setup_resourcer(AZURE_RESOURCES[ENV])
+
+blob_path = AZURE_RESOURCES[ENV]['blob_path']
 
 
-fiserv_handler = TypeHandler(fiserv_types)
+
+
+
+
--- src/info_security.py
+++ src/info_security.py
@@ -1,11 +1,46 @@
+from datetime import datetime as dt
+import pandas as pd
+from pytz import timezone
+
+from epic_py.tools import read_excel_table
 
 
-from epic_py import EpicDataBuilder
-from config import fiserv_handler
+ref_path = "refs/Info Security.xlsx.lnk"
+cols_ref = (ref_path, 'Approach', 'fraud_cols')
+
+meta_cols = read_excel_table(*cols_ref).set_index('columna')
+col_types = meta_cols['tipo'].dropna()
 
 
-fiserv_builder = EpicDataBuilder(type_handler=fiserv_handler)
+def prepare_excelref(xls_df: pd.DataFrame):
+    type_map = {'int': int, 'dbl': float, 'str': str}
+    
+    a_df = (xls_df[col_types.index]
+        .astype(col_types.map(type_map)))
+    return a_df         
 
 
+if __name__ == '__main__': 
+    from config import app_resourcer, blob_path
+    tmp_path = "refs/upload-specs"
+    
+    # table: sheet
+    data_ref = {
+        'payments':  'Payment', 
+        'accounts':  'Account', 
+        'customers': 'Customer'
+        }
 
+    a_ref, sheet = 'payments', 'Payment'
+    for a_ref, sheet in data_ref.items(): 
+        now_str = dt.now(tz=timezone("America/Mexico_City")).strftime("%Y-%m-%d_%H:%M")
 
+        ref_file = f"{tmp_path}/{a_ref}_cols.feather"
+        blob_0   = f"{blob_path}/{a_ref}_specs_latest.feather"
+        blob_1   = f"{blob_path}/{a_ref}_specs_{now_str}.feather"
+
+        pd_ref = read_excel_table(ref_path, sheet, a_ref)
+        as_fthr = prepare_excelref(pd_ref)
+        as_fthr.to_feather(ref_file)
+        app_resourcer.storage_upload_blob(ref_file, blob_0, 'gold', overwrite=True, verbose=1)
+        app_resourcer.storage_upload_blob(ref_file, blob_1, 'gold', overwrite=True, verbose=1)
