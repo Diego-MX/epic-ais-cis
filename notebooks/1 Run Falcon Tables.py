05d4a797df702efa25a50d0efdedf6a235f2fcdd
Diego Villamil <diego.v@bineo.com>
2023-09-07 11:56:36 -0600

    prepare for merge

--- notebooks/1 Run Falcon Tables.py
+++ notebooks/1 Run Falcon Tables.py
@@ -1,39 +1,41 @@
 # Databricks notebook source
-# MAGIC %md 
+# MAGIC %md
 # MAGIC # Tablas de Falcon
 # MAGIC
-# MAGIC En este _notebook_ se ejecutan las actualizaciones de las tablas de Falcon.  
-# MAGIC Las secciones del _notebook_ son:  
-# MAGIC `-1` Desarrollo  
-# MAGIC `0`  Preparación  
-# MAGIC `1.1`  Clientes  
-# MAGIC `1.2`  Ejecutamos las tablas una por una, para tener visibilidad de errores:  
+# MAGIC En este _notebook_ se ejecutan las actualizaciones de las tablas de Falcon.
+# MAGIC Las secciones del _notebook_ son:
+# MAGIC `-1` Desarrollo
+# MAGIC `0`  Preparación
+# MAGIC `1.1`  Clientes
+# MAGIC `1.2`  Ejecutamos las tablas una por una, para tener visibilidad de errores:
 # MAGIC
 # MAGIC * Clientes
 # MAGIC * Cuentas
 # MAGIC * Pagos
 
 # COMMAND ----------
-
-from copy import deepcopy
-from datetime import datetime as dt, date
-import pandas as pd
-from pyspark.sql import SparkSession, functions as F 
-from pyspark.dbutils import DBUtils
-from pytz import timezone
+from datetime import datetime as dt
 import subprocess
-import yaml
+from pytz import timezone
+
+import pandas as pd
+from pyspark.sql import SparkSession, functions as F
+from pyspark.dbutils import DBUtils     # pylint: disable=no-name-in-module,import-error
+import yaml     # pylint: disable=import-error
+
+# pylint: disable=wrong-import-position
+# pylint: disable=wrong-import-order
 
 spark = SparkSession.builder.getOrCreate()
 dbutils = DBUtils(spark)
 
-with open("../user_databricks.yml", 'r') as _f: 
+with open("../user_databricks.yml", 'r') as _f:     # pylint: disable=unspecified-encoding
     u_dbks = yaml.safe_load(_f)
 
 epicpy_load = {
-    'url'   : 'github.com/Bineo2/data-python-tools.git', 
-    'branch': 'dev-diego', 
-    'token' : dbutils.secrets.get(u_dbks['dbks_scope'], u_dbks['dbks_token']) }  
+    'url'   : 'github.com/Bineo2/data-python-tools.git',
+    'branch': 'dev-diego',
+    'token' : dbutils.secrets.get(u_dbks['dbks_scope'], u_dbks['dbks_token']) }
 
 url_call = "git+https://{token}@{url}@{branch}".format(**epicpy_load)
 subprocess.check_call(['pip', 'install', url_call])
@@ -46,29 +48,28 @@ ref_path = Path("../refs/upload-specs")
 # COMMAND ----------
 
 from importlib import reload
-import config; reload(config)
-import epic_py; reload(epic_py)
+import config; reload(config)       # pylint: disable=multiple-statements
+import epic_py; reload(epic_py)     # pylint: disable=multiple-statements
 
-from pathlib import Path
 from epic_py.delta import EpicDF, EpicDataBuilder
 from src.head_foot import headfooters
-from config import (app_agent, app_resourcer, 
-    falcon_handler, falcon_rename, 
+from config import (app_agent, app_resourcer,
+    falcon_handler, falcon_rename,
     dbks_tables, blob_path)
 
 falcon_builder = EpicDataBuilder(typehandler=falcon_handler)
 
-def get_time(tz="America/Mexico_City", time_fmt="%Y-%m-%d"): 
+def get_time(tz="America/Mexico_City", time_fmt="%Y-%m-%d"):
     return dt.now(tz=timezone(tz)).strftime(format=time_fmt)
 
 datalake = app_resourcer['storage']
 dlk_permissions = app_agent.prep_dbks_permissions(datalake, 'gen2')
 app_resourcer.set_dbks_permissions(dlk_permissions)
 
-
+λ_name = lambda row: "{name}-{len}".format(**row)   # pylint: disable=consider-using-f-string
 # COMMAND ----------
 
-# MAGIC %md 
+# MAGIC %md
 # MAGIC ## Clientes
 # MAGIC
 
@@ -78,8 +79,7 @@ cust_time = get_time()
 customers_specs = (pd.read_feather(ref_path/'customers_cols.feather')
         .rename(columns=falcon_rename))
 
-name_onecol = '~'.join(f"[{int(rr['len'])}]{rr['name']}" 
-    for _, rr in customers_specs.iterrows())
+name_onecol = '~'.join(λ_name(rr) for _, rr in customers_specs.iterrows())  # pylint: disable=invalid-name
 
 customers_extract = falcon_builder.get_extract(customers_specs, 'delta')
 customers_loader  = falcon_builder.get_loader(customers_specs, 'fixed-width')
@@ -91,17 +91,17 @@ customers_1= (EpicDF(spark, dbks_tables['gld_client_file'])
     .with_column_plus(customers_extract['_val'])
     .with_column_plus(customers_extract['None']))
 
-customers_2 = (customers_1.select_plus(customers_loader))
+customers_2 = customers_1.select_plus(customers_loader)
 customers_2.display()
 
 customers_3 = (customers_2
     .select(customers_onecol)
-    .prep_one_col(header_info=headfooters[('customer', 'header')], 
+    .prep_one_col(header_info=headfooters[('customer', 'header')],
                  trailer_info=headfooters[('customer', 'footer')]))
 
 customers_3.save_as_file(
-    f"{blob_path}/reports/customers/{cust_time}.csv", 
-    f"{blob_path}/reports/customers/tmp_delta", 
+    f"{blob_path}/reports/customers/{cust_time}.csv",
+    f"{blob_path}/reports/customers/tmp_delta",
     header=False)
 
 
@@ -110,18 +110,10 @@ customers_3.save_as_file(
 print(f"{blob_path}/reports/customers/{cust_time}.csv")
 customers_3.display()
 
-# COMMAND ----------
-
-from epic_py.delta.table_info import FlatFileInfo
-
-info_dict = headfooters[('customer', 'header')].copy()
-info_dict['data']['filler'] = 10
-flatter = FlatFileInfo.create(**info_dict)
-flatter.info_to_row()
 
 # COMMAND ----------
 
-# MAGIC %md 
+# MAGIC %md
 # MAGIC ## Cuentas
 
 # COMMAND ----------
@@ -130,8 +122,7 @@ acct_time = get_time()
 accounts_specs = (pd.read_feather(ref_path/'accounts_cols.feather')
         .rename(columns=falcon_rename))
 
-onecol_account = '~'.join(f"{nm}-{ln}" 
-    for nm, ln in zip(accounts_specs['name'], accounts_specs['len'].astype('int')))
+onecol_account = '~'.join(λ_name(rr) for _, rr in accounts_specs.iterrows())    # pylint: disable=invalid-name
 
 accounts_extract = falcon_builder.get_extract(accounts_specs, 'delta')
 accounts_loader = falcon_builder.get_loader(accounts_specs, 'fixed-width')
@@ -152,12 +143,12 @@ accounts_2.display()
 
 accounts_3 = (accounts_2
     .select(accounts_onecol)
-    .prep_one_col(header_info=headfooters[('account', 'header')], 
+    .prep_one_col(header_info=headfooters[('account', 'header')],
                  trailer_info=headfooters[('account', 'footer')]))
 
 accounts_3.save_as_file(
-    f"{blob_path}/reports/accounts/{acct_time}.csv", 
-    f"{blob_path}/reports/accounts/tmp_delta", 
+    f"{blob_path}/reports/accounts/{acct_time}.csv",
+    f"{blob_path}/reports/accounts/tmp_delta",
     header=False)
 
 # COMMAND ----------
@@ -167,31 +158,31 @@ accounts_3.display()
 
 # COMMAND ----------
 
-# MAGIC %md 
+# MAGIC %md
 # MAGIC ## Pagos
 
 # COMMAND ----------
 
-haz_pagos = False
+haz_pagos = False       # pylint: disable=invalid-name
 
-if haz_pagos: 
+if haz_pagos:
     pymt_time = get_time()
     payments_specs = (pd.read_feather(ref_path/'payments_cols.feather')
             .rename(columns=falcon_rename))
 
+    one_column = '~'.join(λ_name(rr) for _, rr in payments_specs.iterrows())    # pylint: disable=invalid-name
     payments_extract = falcon_builder.get_extract(payments_specs, 'delta')
     payments_loader = falcon_builder.get_loader(payments_specs, 'fixed-width')
     payments_onecol = (F.concat(*payments_specs['name'].values)
-        .alias('one-column'))
+        .alias(one_column))
 
     payments_0 = spark.table(dbks_tables['gld_cx_collections_loans'])
 
     payments_1 = (EpicDF(payments_0)
-        .select([vv.alias(kk) 
+        .select([vv.alias(kk)
             for kk, vv in payments_extract['gld_cx_collections_loans'].items()])
         .with_column_plus(payments_extract['_val'])
-        .with_column_plus(payments_extract['None'])
-        )
+        .with_column_plus(payments_extract['None']))
 
     payments_2 = payments_1.select(payments_loader)
 
