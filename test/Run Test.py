ed5d91b9b85083d41f6dd91cc8c61ef5e78b7d5d
juan-v_bineo <juan-v_bineo>
2024-07-01 16:34:56 +0000

    avance en las pruebas, falta keyvault

--- test/Run Test.py
+++ test/Run Test.py
@@ -6,7 +6,7 @@ import sys
 sys.dont_write_bytecode = True
 
 # Run pytest.
-retcode = pytest.main([".", "-v", "-p", "no:cacheprovider"])
+retcode = pytest.main([".", "-vv", "-p", "no:cacheprovider"])
 print(retcode)
 # Fail the cell execution if there are any test failures.
 assert retcode == 0, "The pytest invocation failed. See the log for details."
--- test/conftest.py
+++ test/conftest.py
@@ -5,7 +5,3 @@ import config as cfg
 def pytest_configure():
     dont_write_bytecode = True
     deps.from_reqsfile(cfg.REQS_FILE)
-    deps.gh_epicpy(cfg.EPIC_REF, cfg.USER_FILE)
-    from epic_py.platform import AzureResourcer
-    from src import (app_agent, app_resourcer,
-                dbks_tables, app_path, app_abfss)
