dfce3e62adc51f52e6b84adc7c914f396a8dd45b
Juan Villalpando <juan.v@bineo.com>
2024-10-25 11:12:35 -0600

    Se bloquea el parche maestro no sirve en PRD

--- notebooks/1 🦝 falcon reports.py
+++ notebooks/1 🦝 falcon reports.py
@@ -362,7 +362,7 @@ customers_onecol  = (F.concat(*customers_specs['name'].values)
     .alias(cis_name))
 
 customers_0 = EpicDF(spark, dbks_tables['clients'])
-customers_0 = parche_tablas(customers_0)
+# customers_0 = parche_tablas(customers_0)  # SE BLOQUEA PORQUE NO SE UTILIZA EN PRD SOLO EN QAS
 
 customers_1 = (one_customers(customers_0)
     .join(x_customers(customers_0), on='client_id')
